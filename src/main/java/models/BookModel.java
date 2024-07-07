a016ecaf2a787d2697e76f8c154f4008b9c277ac
Luzinov Roman <rluzinovtrainee@gmail.com>
2024-07-07 19:02:17 +0300

    Add variable "id" to model book

--- src/main/java/models/BookModel.java
+++ src/main/java/models/BookModel.java
@@ -1,18 +1,26 @@
 package models;
 
 public class BookModel {
+    private int id;
     private int age;
     private String name;
     private String author;
 
     public BookModel(){  }
-    public BookModel (int age, String name, String author){
+    public BookModel (int age, String name, String author, int id){
         this.age = age;
         this.name = name;
         this.author = author;
+        this.id = id;
     }
     public int getAge(){
         return age;
+    }
+    public  int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
     }
 
     public void setAge(int age) {
